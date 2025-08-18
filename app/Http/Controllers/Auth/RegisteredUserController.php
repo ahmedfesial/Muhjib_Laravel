b670b0eb08b3cd89c6f458615c0d6f842fdcd379
HossSalama <hossamsalama706@gmail.com  git config --global user.name Hossam>
2025-08-18 19:28:15 +0300

    Commit

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
             'phone' => $request->phone,
             'role' => $request->role,
-            'image' => $request->image_url
+            'image' => $request->image
         ]);
 
         // Generate token
