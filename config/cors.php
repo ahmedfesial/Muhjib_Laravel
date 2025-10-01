fecbef4a0bd95300ff8cbf5a4fd385ca09c067cf
ahmedfesial <ahmedfesial150@gmail.com>
2025-10-01 19:35:08 +0300

    update

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,12 @@ return [
     |
     */
 
-    'paths' => ['*'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
-
+    'allowed_origins' => ['https://surfaces-care-qam8.vercel.app'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
-
+    'supports_credentials' => false,
 ];
